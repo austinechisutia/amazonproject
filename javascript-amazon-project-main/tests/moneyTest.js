e086fa399db127fb906f320595002bcbe7486df2
austinechisutia <106369577+austinechisutia@users.noreply.github.com>
2025-01-01 14:21:06 +0300

    create automated test for formatt currency

---
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -0,0 +1,22 @@
+import {formatMoney} from '../script/money.js';
+
+if(formatMoney(2095) === '$20.95') {
+    console.log('test passed');
+} else {
+    console.log('test failed');
+}
+console.log("Works with zero");
+
+console.log("Converts cents into dollars");
+if(formatMoney(0) === '$0.00') {
+    console.log('test passed');
+} else {    
+    console.log('test failed');
+}
+
+console.log("rounds to the nearest zero")
+if(formatMoney(2000.5) === '$20.01') {
+    console.log('test passed');
+} else {
+    console.log('test failed');
+}
